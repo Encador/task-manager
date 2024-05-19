587f4c19c86f0fbef32e00c81c31d01517fd6ce9
encador <encador@proton.me>
2024-05-19 18:05:47 -0400

    chore: delete template code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <body>
+  <h1>Task Manager</h1>
+  <div class="container">
+    <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 2</li>
+    </ul>
+  </div>
+  </body>
 </template>
+
+<style scoped>
+* {
+  margin: 0;
+}
+body h1{
+  text-align: center;
+}
+
+
+
+
+</style>
