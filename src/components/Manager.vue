93d2439ab07985ceb750615db1cf480adcc20259
encador <encador@proton.me>
2024-05-19 18:45:52 -0400

    feat: add manager component Manager component will be responsible for displaying, adding and filtering tasks

---
+++ src/components/Manager.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <ul class="task-list">
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 2</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background-color: #f4f4f4;
+
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,17 @@
 <script setup lang="ts">
+import Manager from "@/components/Manager.vue";
 </script>
 
 <template>
-  <body>
   <h1>Task Manager</h1>
-  <div class="container">
-    <ul>
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 2</li>
-    </ul>
-  </div>
-  </body>
+  <Manager/>
 </template>
 
 <style scoped>
-* {
-  margin: 0;
-}
-body h1{
+
+h1{
   text-align: center;
 }
-
 
 
 
