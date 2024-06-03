c1ba82df4e842359bee92129b1100cac6ca8da10
encador <encador@proton.me>
2024-06-03 00:18:35 -0400

    add ability to sign in

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,32 +1,40 @@
 <script setup lang="ts">
 import {pb, currentUser} from "@/assets/pocketbase";
+import {ref} from "vue";
+
+const username = ref<string>("")
+const password = ref<string>("")
 
 async function login(){
-  try {
-    await pb.collection("users").authWithPassword("user1","password")
-    console.log("login")
-  }
-  catch(err){
-    console.log(err);
-  }
-
+  await pb.collection("users").authWithPassword(username.value,password.value).then().catch((e:Error) => console.error(e));
 }
 async function logout(){
   pb.authStore.clear();
-  console.log("logout")
-
 }
 
 </script>
 
 <template>
-  <h1 v-if="currentUser">Signed in as {{currentUser.username}}</h1>
-  <h1 v-else>Not Signed In</h1>
-  <button @click="login">Login</button>
-  <button @click="logout">Logout</button>
+  <div v-if="currentUser">
+    <h1>Signed in as {{currentUser.username}}</h1>
+    <button @click="logout">Logout</button>
+  </div>
+  <div v-else>
+    <h1>Not Signed In</h1>
+    <input type="text" v-model="username" placeholder="Username" />
+    <input type="password" v-model="password" placeholder="Password" />
+    <button @click="login">Login</button>
+  </div>
+
+
 
 </template>
 
 <style scoped>
+
+div{
+  margin: 10px;
+  padding: 2px;
+}
 
 </style>
